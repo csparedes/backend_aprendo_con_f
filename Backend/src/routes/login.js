64b490f49cd0dc0f3cdb0c1dc1b009da08154e7b
xavier <xavier.cobos.29@hotmail.com>
2023-12-08 23:06:01 -0500

    Revert "Descrypt password para login de usuario, variación en mensajes de salida login"
    This reverts commit 9a4408635f3090a1df1ffe2bfc6e6d087cf605bc.

--- Backend/src/routes/login.js
+++ Backend/src/routes/login.js
@@ -1,45 +1,31 @@
 const router = require('express').Router();
 const jwt = require('jsonwebtoken');
-const db = require('../config/db');
-const bcrypt = require('bcryptjs');
-const UserModel = require('../models/user.model');
+const db=require('../config/db')
 
 // Ruta para iniciar sesión (POST)
 router.post('/login', async (req, res) => {  
   const user_data = req.body; // {} --> objeto vacio pero existe!!!
   const dataVerify = Object.values(user_data) // [] --> [datos,datos,datos]
-  //console.log(user_data);
+  console.log(user_data);
   
   try {
+      console.log('Entra al servicio');
      // si es igual o existe en la base de datos ingresa
     const resultado = await db.promise().query('SELECT * FROM teacher_app.user WHERE email = ?', [user_data.email]);
+    console.log(resultado);
+    
     const rows = resultado[0];
-    const password = user_data.password;
     const token = jwt.sign({ user_data }, 'secreto', { expiresIn: '1h' });
-    //Revisión de email en base de datos;
-    const [result] = await UserModel.verificaCorreo( user_data.email, 'El correo existe', 'El correo no existe');
-    const valores = Object.values(result[0]); //El correo existe o no existe
-    if (valores == 'El correo no existe') {
-        return res.json({
-                respuesta: false,
-                mensaje: 'correo o password incorrectos',
-                resultado: null
-        });
-      }  
-    const equals = bcrypt.compareSync(password, rows[0].password)
-    if (!equals) {
-            return res.json({
-                    respuesta: false,
-                    mensaje: 'correo o password incorrectos',
-                    resultado: null
-            });
-        }   
-        res.json({
-                    respuesta: true,
-                    mensaje: 'Login corecto',
-                    resultado: token
-            }); 
+    const check_email_value = rows.filter(row => row.email === dataVerify[0] && row.password === dataVerify[1]);
+
+    if (check_email_value.length > 0 ) {  // si es igual o existe en la base de datos ingresa
+        res.status(200).json({ message:token });
+        console.log('Ingreso exitoso')
+      } else {
+        res.status(401).json({ message: 'Credenciales incorrectas' });
+      }
   } catch (error) {
+    console.log('Entra al servicio');
     res.status(401).send({err:error.message})
   }
   
