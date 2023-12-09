6e88a2854907b8b14cd620c697d941b2b681aa8e
xavier <xavier.cobos.29@hotmail.com>
2023-12-09 00:01:55 -0500

    Cambio en respuestas serivcio login

--- Backend/src/routes/login.js
+++ Backend/src/routes/login.js
@@ -1,23 +1,47 @@
 const router = require('express').Router();
 const jwt = require('jsonwebtoken');
-const db=require('../config/db')
+const db = require('../config/db');
+const bcrypt = require('bcryptjs');
+const UserModel = require('../models/user.model');
 
 // Ruta para iniciar sesión (POST)
-router.post('/login', async(req, res) => {
-  const user_data = req.body; // {} --> objeto vacio pero existe!!!
-  const dataVerify=Object.values(user_data) // [] --> [datos,datos,datos]
+router.post('/login', async (req, res) => {  
+const user_data = req.body; // {} --> objeto vacio pero existe!!!
+  console.log(user_data); 
   try {
-    const [rows] = await db.promise().query('SELECT * FROM teacher_app.user'); //verificar que este aqui mismo
-      const token = jwt.sign({ user_data }, 'secreto', { expiresIn: '1h' }); //lo que envian desde el front, la llave secreta y el tiempo de expiracion
-      const check_username_value=rows.filter((e)=> e.username === dataVerify[0]) //Crea un array
-    if (check_username_value.length > 0 ) { // si es igual o existe en la base de datos ingresa
-        res.status(200).json({ message:token });
-        console.log('Ingreso exitoso')
-      } else {
-        res.status(401).json({ message: 'Credenciales incorrectas' });
-      }
-      
+      console.log('Entra al servicio');
+     // si es igual o existe en la base de datos ingresa
+    const resultado = await db.promise().query('SELECT * FROM teacher_app.user WHERE email = ?', [user_data.email]);
+    console.log(resultado);
+    const rows = resultado[0];
+    const password = user_data.password;
+    const token = jwt.sign({ user_data }, 'secreto', { expiresIn: '1h' });
+
+   //Revisión de email en base de datos;
+    const [result] = await UserModel.verificaCorreo( user_data.email, 'El correo existe', 'El correo no existe');
+    const valores = Object.values(result[0]); //El correo existe o no existe
+    if (valores == 'El correo no existe') {
+        return res.json({
+                respuesta: false,
+                mensaje: 'correo o password incorrectos',
+                resultado: null
+        });
+      }  
+    const equals = bcrypt.compareSync(password, rows[0].password)
+    if (!equals) {
+            return res.json({
+                    respuesta: false,
+                    mensaje: 'correo o password incorrectos',
+                    resultado: null
+            });
+        }   
+        res.json({
+                    respuesta: true,
+                    mensaje: 'Login Correcto',
+                    resultado: token
+            }); 
   } catch (error) {
+    console.log('Entra al servicio');
     res.status(401).send({err:error.message})
   }
   
