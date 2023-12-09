cbde06d344c6ce80cb7ec53790cd0d70bcc51aa0
MigFerF01 <migfer212@gmail.com>
2023-12-08 22:49:19 -0500

    Token

--- Backend/src/routes/login.js
+++ Backend/src/routes/login.js
@@ -7,13 +7,13 @@ router.post('/login', async(req, res) => {
   const user_data = req.body; // {} --> objeto vacio pero existe!!!
   const dataVerify=Object.values(user_data) // [] --> [datos,datos,datos]
   try {
-    const resultado = await db.promise().query('SELECT * FROM teacher_app.user WHERE username = ?', [user_data.username]);
+    const resultado = await db.promise().query('SELECT * FROM teacher_app.user WHERE email = ?', [user_data.email]);
     const rows = resultado[0];
     const token = jwt.sign({ user_data }, 'secreto', { expiresIn: '1h' });
-    const check_username_value = rows.filter(row => row.username === dataVerify[0] && row.password === dataVerify[1]);
+    const check_username_value = rows.filter(row => row.email === dataVerify[0] && row.password === dataVerify[1]);
     
     if (check_username_value.length > 0 ) { 
-        res.status(200).json({ message:token });
+        res.status(200).json({ token:token });
         console.log('Ingreso exitoso')
       } else {
         res.status(401).json({ message: 'Credenciales incorrectas' });
